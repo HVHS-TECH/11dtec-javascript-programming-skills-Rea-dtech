436c2783d4998b6b05274bdc012f65469901c725
Rea-dtech <24582rd@hvhs.school.nz>
2025-04-29 23:49:17 +0000

    Apr 29, 2025, 11:49 PM

--- t02_variables.js
+++ t02_variables.js
@@ -1,12 +1,15 @@
 //variables by rea 29/4/4 the purpose of this code is to learn js on it
 
-
 //variables
 let myname = "";
-
-
 
 //main code
 console.log("my name is " + myname);
 
 //functions
+function updateName() {
+    const inputElement = document.getElementById("nameInput");
+    myname = inputElement.value;
+    console.log("my name is " + myname);
+    document.getElementById("output").innerText = "My name is " + myname;
+}
