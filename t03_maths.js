056c27fe1fb596f44f9d7103b5eabd2a67661fa0
Rea-dtech <24582rd@hvhs.school.nz>
2025-04-30 00:10:09 +0000

    Apr 30, 2025, 12:10 AM

--- t03_maths.js
+++ t03_maths.js
@@ -2,10 +2,13 @@
 
 //variables
 let name = "rea";
-let age ="14";
+let age ="15";
 let money ="100";
 let year = "2025";
 //main code
 console.log ("Hi " + name + " as of " + year + " you are " + age + " and you have " + money + " dollars");
-console.log ("you were born in " + " ")
+console.log ("you were born in ");
+console.log (year - age);
+console.log (" in 10 years you will be " );
+console.log (15 + 10)
 //functions
